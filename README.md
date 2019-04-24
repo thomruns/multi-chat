cd3834a1c3644f34a92fd8f484e6bbab7be3053b
Thom David <thomdavid74@gmail.com>
2019-04-24 10:35:45 -0700

    Chatroom Class constructor, addChat method

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Multi-chat
 
 A simple chat application featuring multi rooms, using Firebase Firestore for the backend. Bootstrap is used for styling.
+
+Firebase requires an API key, which should be included in the appropriate field in the config object in the app.js file.
--- scripts/chat.js
+++ scripts/chat.js
@@ -1,1 +1,35 @@
-console.log('chat.js');
+// The Chatroom Class
+
+// setting up a real-time listener to get new chats
+
+// updating the username
+
+// updating the room
+
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection('chats');
+  }
+  // adding new chat documents
+  async addChat(message) {
+    // format a chat object
+    const now = new Date();
+    const chat = {
+      message: message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    // save chat document to database
+    const response = await this.chats.add(chat);
+    return response;
+  }
+}
+
+const chatroom = new Chatroom('movies', 'thom');
+// testing only
+// chatroom.addChat('This is a new movies message')
+//   .then(() => console.log('new message added'))
+//   .catch(err => console.log(err));
