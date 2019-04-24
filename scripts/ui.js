410c14e04f1f3030cdfe181e04f2a02038e355e9
Thom David <thomdavid74@gmail.com>
2019-04-24 15:11:46 -0700

    Add DateFNS for date format, update local styles

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,6 +1,6 @@
 // UIChat Class
 
-// render chat templates to the DOM
+
 
 // clear the (DOM) list of chats when the room changes
 
@@ -8,12 +8,17 @@ class ChatUI {
   constructor(list) {
     this.list = list;
   }
+  // render chat templates to the DOM method
   render(data) {
+    const when = dateFns.distanceInWordsToNow(
+      data.created_at.toDate(),
+      { addSuffix: true }
+    );
     const html = `
       <li class="list-group-item">
         <span class="username">${data.username}</span>
         <span class="message">${data.message}</span>
-        <div class="time">${data.created_at.toDate()}</div>
+        <div class="time">${when}</div>
       </li>
     `;
     this.list.innerHTML += html;
