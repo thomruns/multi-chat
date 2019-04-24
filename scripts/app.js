7d812b4507c59f54930b81c217890a7d4ee650cb
Thom David <thomdavid74@gmail.com>
2019-04-24 10:09:04 -0700

    Firebase API setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-console.log('app.js');
+  // Initialize Firebase
+  var config = {
+    apiKey: "API_KEY_HERE",
+    authDomain: "udemy-modernjs.firebaseapp.com",
+    databaseURL: "https://udemy-modernjs.firebaseio.com",
+    projectId: "udemy-modernjs",
+    storageBucket: "udemy-modernjs.appspot.com",
+    messagingSenderId: "626828143138"
+  };
+  firebase.initializeApp(config);
+
+  const db = firebase.firestore(); // reference the database
